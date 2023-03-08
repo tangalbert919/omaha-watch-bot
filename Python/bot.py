ee88bf34a5e381b1446ac750a56cedbfeadef1cc
Albert Tang <tangalbert919@yahoo.com>
2023-03-08 15:18:39 -0600

    Python: Switch to Google's Version History API
    Omaha Proxy is deprecated and shutting down at the end of this month.

--- Python/bot.py
+++ Python/bot.py
@@ -17,13 +17,13 @@ class WatchBot(commands.Bot):
     async def on_ready(self):
         print('Logged in')
         async with aiohttp.ClientSession() as session:
-            async with session.get('https://omahaproxy.appspot.com/all.json?os=win') as resp:
+            async with session.get('https://versionhistory.googleapis.com/v1/chrome/platforms/win/channels/all/versions/all/releases?filter=endtime=none&order_by=fraction%20desc') as resp:
                 data = await resp.json()
             await session.close()
-        self.stable_version = data[0]['versions'][4]['version']
-        self.beta_version = data[0]['versions'][3]['version']
-        self.dev_version = data[0]['versions'][2]['version']
-        self.canary_version = data[0]['versions'][1]['version']
+        self.stable_version = data['releases'][0]['version']
+        self.beta_version = data['releases'][1]['version']
+        self.dev_version = data['releases'][2]['version']
+        self.canary_version = data['releases'][3]['version']
         self.loop.create_task(self.fetch_omaha())
 
     async def on_message(self, message):
@@ -34,26 +34,26 @@ class WatchBot(commands.Bot):
         # This function runs every hour.
         while not self.is_closed():
             async with aiohttp.ClientSession() as session:
-                async with session.get('https://omahaproxy.appspot.com/all.json?os=win') as resp:
+                async with session.get('https://versionhistory.googleapis.com/v1/chrome/platforms/win/channels/all/versions/all/releases?filter=endtime=none&order_by=fraction%20desc') as resp:
                     data = await resp.json()
                 await session.close()
-            if data[0]['versions'][4]['version'] != self.stable_version:
-                self.stable_version = data[0]['versions'][4]['version']
+            if data['releases'][0]['version'] != self.stable_version:
+                self.stable_version = data['versions'][0]['version']
                 embed = discord.Embed(title='Stable update available!', color=discord.Colour.green())
                 embed.add_field(name='New version: ', value=self.stable_version, inline=False)
                 await self.sendEmbed(embed, title='Chromium Stable Channel')
-            if data[0]['versions'][3]['version'] != self.beta_version:
-                self.beta_version = data[0]['versions'][3]['version']
+            if data['releases'][1]['version'] != self.beta_version:
+                self.beta_version = data['versions'][1]['version']
                 embed = discord.Embed(title='Beta update available!', color=discord.Colour.gold())
                 embed.add_field(name='New version: ', value=self.beta_version, inline=False)
                 await self.sendEmbed(embed, title='Chromium Beta Channel')
-            if data[0]['versions'][2]['version'] != self.dev_version:
-                self.dev_version = data[0]['versions'][2]['version']
+            if data['releases'][2]['version'] != self.dev_version:
+                self.dev_version = data['versions'][2]['version']
                 embed = discord.Embed(title='Dev update available!', color=discord.Colour.red())
                 embed.add_field(name='New version: ', value=self.dev_version, inline=False)
                 await self.sendEmbed(embed, title='Chromium Dev Channel')
-            if data[0]['versions'][1]['version'] != self.canary_version:
-                self.canary_version = data[0]['versions'][1]['version']
+            if data['releases'][3]['version'] != self.canary_version:
+                self.canary_version = data['versions'][3]['version']
                 embed = discord.Embed(title='Canary update available!', color=discord.Colour.purple())
                 embed.add_field(name='New version: ', value=self.canary_version, inline=False)
                 await self.sendEmbed(embed, title='Chromium Canary Channel')
--- README.txt
+++ README.txt
@@ -1,3 +1,3 @@
-This discord bot notifies me of any new updates from the Omaha Proxy.
+This discord bot notifies me of any new updates from Google's Version History API.
 
 Currently, it just watches all the release branches (except canary_asan) for Windows x64.
