11e6be28a6a8e45ee0a4a6d90c2c466b5728eaf3
Albert Tang <tangalbert919@yahoo.com>
2023-03-22 09:12:09 -0500

    Python: Fix seting chromium versions internally

--- Python/bot.py
+++ Python/bot.py
@@ -38,22 +38,22 @@ class WatchBot(commands.Bot):
                     data = await resp.json()
                 await session.close()
             if data['releases'][0]['version'] != self.stable_version:
-                self.stable_version = data['versions'][0]['version']
+                self.stable_version = data['releases'][0]['version']
                 embed = discord.Embed(title='Stable update available!', color=discord.Colour.green())
                 embed.add_field(name='New version: ', value=self.stable_version, inline=False)
                 await self.sendEmbed(embed, title='Chromium Stable Channel')
             if data['releases'][1]['version'] != self.beta_version:
-                self.beta_version = data['versions'][1]['version']
+                self.beta_version = data['releases'][1]['version']
                 embed = discord.Embed(title='Beta update available!', color=discord.Colour.gold())
                 embed.add_field(name='New version: ', value=self.beta_version, inline=False)
                 await self.sendEmbed(embed, title='Chromium Beta Channel')
             if data['releases'][2]['version'] != self.dev_version:
-                self.dev_version = data['versions'][2]['version']
+                self.dev_version = data['releases'][2]['version']
                 embed = discord.Embed(title='Dev update available!', color=discord.Colour.red())
                 embed.add_field(name='New version: ', value=self.dev_version, inline=False)
                 await self.sendEmbed(embed, title='Chromium Dev Channel')
             if data['releases'][3]['version'] != self.canary_version:
-                self.canary_version = data['versions'][3]['version']
+                self.canary_version = data['releases'][3]['version']
                 embed = discord.Embed(title='Canary update available!', color=discord.Colour.purple())
                 embed.add_field(name='New version: ', value=self.canary_version, inline=False)
                 await self.sendEmbed(embed, title='Chromium Canary Channel')
